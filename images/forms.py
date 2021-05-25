5d00e053c49b1d65f7b4a809f439c25b144b7ee2
Julio <julesc00@hotmail.com>
2021-05-24 22:33:11 -0500

    Added image download capabilities.

--- images/forms.py
+++ images/forms.py
@@ -1,4 +1,8 @@
 from django import forms
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
+
+from urllib import request
 
 from .models import Image
 
@@ -23,3 +27,20 @@ class ImageCreateForm(forms.ModelForm):
             raise forms.ValidationError(
                 "The given URL doesn't match valid image extensions.")
         return url
+
+    def save(self, force_insert=False, force_commit=False, commit=True):
+        """Override save in order to retrieve the given image and save it."""
+
+        image = super().save(commit=False)
+        image_url = self.cleaned_data["url"]
+        name = slugify(image.title)
+        extension = image_url.rsplit(".", 1)[1].lower()
+        image_name = f"{name}.{extension}"
+
+        # Download image from the given URL.
+        res = request.urlopen(image_url)
+        image.image.save(image_name, ContentFile(res.read()), save=False)
+
+        if commit:
+            image.save()
+        return image
